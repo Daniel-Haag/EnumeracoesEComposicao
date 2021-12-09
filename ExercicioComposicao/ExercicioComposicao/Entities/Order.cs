10f4442f48cd6fd92e0736ca4e38beb140c176f9
Daniel Haag <danni.haag91@gmail.com>
2021-12-09 18:42:35 -0300

    primeiro commit

---
+++ ExercicioComposicao/ExercicioComposicao/Entities/Order.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioComposicao.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public Client Client { get; set; }
+
+        public List<OrderItem> orderItems = new List<OrderItem>();
+
+        public Order()
+        {
+
+        }
+        public Order(OrderStatus status, Client client)
+        {
+            Moment = DateTime.Now;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            orderItems.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            orderItems.Remove(item);
+        }
+
+        public double Total()
+        {
+            double total = 0;
+
+            foreach (var item in orderItems)
+            {
+                total += item.subTotal();
+            }
+
+            return total;
+        }
+    }
+}
---
+++ ExercicioComposicao/ExercicioComposicao/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using ExercicioComposicao.Entities;
+
+namespace ExercicioComposicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter client data:");
+            Console.Write("Name:");
+            string name = Console.ReadLine();
+            Console.Write("Email:");
+            string email = Console.ReadLine();
+            Console.Write("Birth Date (DD/MM/YYYY):");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+            Client client = new Client(name, email, birthDate);
+
+            Console.WriteLine("Enter order Data:");
+            Console.Write("Status - (PendingPayment/Processing/Shipped/Delivered):");
+            OrderStatus orderStatus = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Order order = new Order(orderStatus, client);
+
+            Console.Write("How many items to this order?");
+            int quantity = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < quantity; i++)
+            {
+                Console.WriteLine($"Enter #{i+1} item data:");
+                Console.Write("Product Name:");
+                string productName = Console.ReadLine();
+                Console.Write("Product Price:");
+                double productPrice = double.Parse(Console.ReadLine());
+                Console.Write("Quantity:");
+                int productQuantity = int.Parse(Console.ReadLine());
+
+                Product product = new Product(productName, productPrice);
+                OrderItem orderItem = new OrderItem(productQuantity, productPrice, product);
+
+                orderItem.subTotal();
+
+                order.AddItem(orderItem);
+            }
+
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine($"Order moment: {order.Moment}");
+            Console.WriteLine("Order status: " + order.Status.ToString());
+            Console.WriteLine($"Client: {order.Client.Name} - {order.Client.BirthDate} - {order.Client.Email}");
+            Console.WriteLine("Order items:");
+
+            foreach (var item in order.orderItems)
+            {
+                Console.Write($"{item.Product.Name}, ${item.Product.Price}, Quantity: {item.Quantity}, SubTotal: {item.subTotal()}");
+            }
+
+
+        }
+    }
+}
